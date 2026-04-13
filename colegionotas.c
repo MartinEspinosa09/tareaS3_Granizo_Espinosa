9c3983b1bae8eade6135476510e32d500f65978e
Martin Espinosa <felipe.espinosa@udla.edu.ec>
2026-04-13 10:08:36 -0500

    correccion de promedios y logica de las notas

--- colegionotas.c
+++ colegionotas.c
@@ -5,8 +5,13 @@ int main (int argc, char *argv[]) {
     int numEstudiantes, numAsignaturas;
     float sumaEstudiante, sumaAsignatura;
     float notas[5][3];
+<<<<<<< HEAD
     float notaMax= notas[0][0];
     float notaMin= notas[0][0];
+=======
+    float notaMax= 0;
+    float notaMin= 10;
+>>>>>>> 4d22eca (correccion de promedios y logica de las notas)
     int aprobados=0;
 
     for (int i = 0; i < 5; i++)
@@ -40,6 +45,7 @@ int main (int argc, char *argv[]) {
             sumaEstudiante += notas[i][j];
         }
         
+<<<<<<< HEAD
         printf("el promedio de los estudiantes es: %.2f\n", sumaEstudiante/3.0);
         
         if ((sumaEstudiante/3.0)<6)
@@ -49,6 +55,17 @@ int main (int argc, char *argv[]) {
         else
         {
             printf("el estudiante ha aprobado con un promedio de: %2.f \n", i + 1, sumaEstudiante/3.0);
+=======
+        printf("el promedio del estudiante es: %.2f\n", sumaEstudiante/3.0);
+        
+        if ((sumaEstudiante/3.0)<6)
+        {
+            printf("el estudiante %d a reprobado con un promedio de: %.2f \n", i + 1, sumaEstudiante/3.0);
+        }
+        else
+        {
+            printf("el estudiante %d a aprobado con un promedio de: %2.f \n", i + 1, sumaEstudiante/3.0);
+>>>>>>> 4d22eca (correccion de promedios y logica de las notas)
             aprobados++;
         }
         
@@ -68,6 +85,7 @@ int main (int argc, char *argv[]) {
 
     }
     
+<<<<<<< HEAD
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -90,4 +108,7 @@ int main (int argc, char *argv[]) {
 
     printf("\n el total de aprobados entre los estudiantes fueron: %.2f", aprobados);
     return 0;
+=======
+   
+>>>>>>> 4d22eca (correccion de promedios y logica de las notas)
 }
