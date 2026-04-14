513a09398acb2435418f184c57ada5efa3d802d2
Martin Espinosa <felipe.espinosa@udla.edu.ec>
2026-04-14 10:47:49 -0500

    trabajo final

--- colegionotas.c
+++ colegionotas.c
@@ -1,3 +1,12 @@
+/*INTEGRANTES: Jair Granizo y Martin Espinosa
+Fecha: 14-4-26
+Descripcion: Una escuela desea implementar un programa en C que le permita
+gestionar las calificaciones de un grupo de estudiantes en varias asignaturas.Además de gestionar
+las calificaciones, el programa debe calcular y mostrar información relevante como el promedio
+de calificaciones por estudiante y por asignatura, la calificación más alta y baja, y cuántos
+estudiantes aprobaron cada asignatura.
+*/
+
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
